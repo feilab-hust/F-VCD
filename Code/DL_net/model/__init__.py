d34bf9d12a60dd75002a7c74c7538a5444cd47d8
Luis <41307097+LuisFMCuriel@users.noreply.github.com>
2024-01-23 09:23:52 +0100

    Update __init__.py
    Only import the models presented in unet_inception.py

--- Code/DL_net/model/__init__.py
+++ Code/DL_net/model/__init__.py
@@ -1,3 +1,4 @@
-from .unet_inception import MultiRes_UNet,MultiRes_UNetX5_crossDynamic,MultiRes_UNeT_test
+#from .unet_inception import MultiRes_UNet,MultiRes_UNetX5_crossDynamic,MultiRes_UNeT_test
+from .unet_inception import MultiRes_UNeT_test
 from .LF_attenton_denoise import LF_attention_denoise
 from .util import *
